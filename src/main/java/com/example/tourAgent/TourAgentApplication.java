47ff43a1741447095a89624d67ee3c2c225727db
Chinedu Victor <nwachukwuvictorc@gmil.com>
2021-02-10 13:37:26 +0100

    commit to main

---
+++ src/main/java/com/example/tourAgent/TourAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.tourAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tourAgent/TourAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tourAgent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
